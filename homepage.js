013d47f61247fd28ea449874c24c8e83a302dbbe
Jane16Frost <96366190+Jane16Frost@users.noreply.github.com>
2022-07-26 22:15:16 -0600

    Add files via upload

--- homepage.js
+++ homepage.js
@@ -2,3 +2,14 @@ $(".nav-item").click(function(){
   $('.navbar-collapse').addClass('collapse');
   $('.navbar-collapse').removeClass('show');
 });
+
+$(".packageBtn").click(function()
+{
+  $(".PopUp").removeClass('hidden');
+
+});
+
+$(".closeBtn").click(function()
+{
+    $(".PopUp").addClass('hidden');
+});
